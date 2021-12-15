49175730e8387500c0d31838d16653fc9657756a
alex <a.korneyko@gmail.com>
2021-12-15 18:34:54 +0100

    Bug-fix with links creation

--- src/lib/routing/i18n-router-link.directive.ts
+++ src/lib/routing/i18n-router-link.directive.ts
@@ -7,7 +7,7 @@ import {I18nOptions} from "../i18nOptions";
 @Directive({
   selector: '[i18nRouterLink]'
 })
-export class I18nRouterLinkDirective extends RouterLinkWithHref implements AfterViewInit{
+export class I18nRouterLinkDirective extends RouterLinkWithHref implements AfterViewInit {
 
   @Input()
   i18nRouterLink = "";
@@ -24,11 +24,13 @@ export class I18nRouterLinkDirective extends RouterLinkWithHref implements After
       return;
     }
 
-    let hrefLangLink: string;
+    // let hrefLangLink: string;
 
-    hrefLangLink = this.i18nService.IsDefaultLang(this.i18nService.currentLangTag)
-      ? (this.i18nRouterLink.startsWith("/") ?  this.i18nRouterLink : "/" + this.i18nRouterLink)
-      : "/" + this.i18nService.currentLangTag + (this.i18nRouterLink.startsWith("/") ?  this.i18nRouterLink : "/" + this.i18nRouterLink);
+    let hrefLangLink = this.i18nService.IsDefaultLang(this.i18nService.currentLangTag)
+      ? (this.i18nRouterLink.startsWith("/")
+        ? this.i18nRouterLink
+        : "/" + this.i18nRouterLink)
+      : "/" + this.i18nService.currentLangTag + (this.i18nRouterLink.startsWith("/") ? this.i18nRouterLink : "/" + this.i18nRouterLink);
 
     if (hrefLangLink.endsWith("/")) {
       hrefLangLink = hrefLangLink.substr(0, hrefLangLink.length - 1);
@@ -38,12 +40,13 @@ export class I18nRouterLinkDirective extends RouterLinkWithHref implements After
     super.routerLink = hrefLangLink;
 
     this.i18nService.AddLanguageChangeListener(langTag => {
-      if (this.i18nService.IsDefaultLang(langTag)){
+      if (this.i18nService.IsDefaultLang(langTag)) {
         super.routerLink = this.i18nRouterLink;
-        nativeElement.href = this.i18nRouterLink + "" ?? "";
+        nativeElement.href = this.i18nRouterLink;
       } else {
-        super.routerLink = langTag + "/" + this.i18nRouterLink;
-        nativeElement.href = langTag + "/" + this.i18nRouterLink;
+        let linkWithLang = "/" + langTag + (this.i18nRouterLink.startsWith("/") ? "" : "/") + this.i18nRouterLink;
+        super.routerLink = linkWithLang;
+        nativeElement.href = linkWithLang;
       }
       if (nativeElement.href.endsWith("/")) {
         nativeElement.href = nativeElement.href.substr(0, nativeElement.href.length - 1)
